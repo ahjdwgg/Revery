aeb362fb469cefeef0977eded174acc0e8b1e1a1
rainasong <rainasong0509@gmail.com>
2021-11-05 21:39:23 +1300

    refactor: moved svg to components

---
+++ components/buttons/icons/plus.tsx
@@ -0,0 +1,14 @@
+const Plus = () => (
+    <svg
+        className="fill-current"
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M9.66658 4.33325H5.66658V0.333252H4.33325V4.33325H0.333252V5.66659H4.33325V9.66659H5.66658V5.66659H9.66658V4.33325Z" />
+    </svg>
+);
+
+export default Plus;
