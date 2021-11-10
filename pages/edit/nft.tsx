2690621882a2b974fe97e0eab6f10fcd26ee25da
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2021-11-10 23:13:50 +1300

    fix: asset card height

--- pages/edit/nft.tsx
+++ pages/edit/nft.tsx
@@ -14,12 +14,11 @@ const NFT = () => {
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
             </Head>
-            <div className="h-full flex flex-col">
+            <div className="h-full">
                 <Header></Header>
-
                 <div className="pt-12 md:pt-16 flex flex-col max-w-7xl m-auto h-full">
                     <h1 className="mt-4 font-bold text-left text-primary text-lg">Edit NFTs</h1>
-                    <section className="flex flex-col w-full h-full flex-1 items-center pt-10 pb-8">
+                    <section className="flex flex-col w-full h-0 flex-1 items-center pt-10 pb-8">
                         <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-full gap-4">
                             <AssetCard
                                 title="Listed"
@@ -29,7 +28,7 @@ const NFT = () => {
                                 footerButton="Unlist All"
                             >
                                 <div className="w-full mb-auto flex-shrink-0 grid gap-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 2xl:grid=cols-6 justify-items-center">
-                                    {[...Array(12)].map((_, i) => (
+                                    {[...Array(120)].map((_, i) => (
                                         <div
                                             key={i}
                                             className="flex items-center justify-center relative m-auto cursor-move"
@@ -41,7 +40,7 @@ const NFT = () => {
                             </AssetCard>
                             <AssetCard title="Unlisted" color="nft" isShowingEditButton={true} footerButton="List All">
                                 <div className="w-full mb-auto flex-shrink-0 grid gap-6 grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 2xl:grid=cols-6 justify-items-center">
-                                    {[...Array(12)].map((_, i) => (
+                                    {[...Array(120)].map((_, i) => (
                                         <div
                                             key={i}
                                             className="flex items-center justify-center relative m-auto cursor-move"
