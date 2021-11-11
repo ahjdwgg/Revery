fcef1f00ef54b5a384a543daed94678625a1390b
Nya Candy <dev@candinya.com>
2021-11-11 14:14:23 +0800

    fix: inner image size

--- components/ImageHolder.tsx
+++ components/ImageHolder.tsx
@@ -12,15 +12,8 @@ const ImageHolder = ({ imageUrl, title, isFullRound, size }: ImageProps) => {
     const roundedStyleString = isFullRound ? 'rounded-full' : 'rounded';
 
     return (
-        <div className={`flex justify-around ${roundedStyleString}`}>
-            <Image
-                className={roundedStyleString}
-                src={imageUrl}
-                alt={title}
-                width={size}
-                height={size}
-                objectFit={'cover'}
-            />
+        <div className={`flex justify-around ${roundedStyleString}`} style={{ width: size, height: size }}>
+            <Image className={roundedStyleString} src={imageUrl} alt={title} layout="fill" objectFit="cover" />
         </div>
     );
 };
