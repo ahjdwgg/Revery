bc8779446f45224b9c2cea1392737d3c1f87f987
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2021-11-16 04:07:32 +1300

    add: animation

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { BiX } from 'react-icons/bi';
 
 interface ModalProps {
@@ -9,24 +9,36 @@ interface ModalProps {
 }
 
 export default function Modal({ theme, hidden, children, closeEvent }: ModalProps) {
+    const [animation, setAnimation] = useState(true);
+
+    const modalClose = () => {
+        setAnimation(false);
+        setTimeout(() => {
+            closeEvent();
+            setAnimation(true);
+        }, 500);
+    };
+
     return (
         <div
-            className={`absolute top-0 z-50 w-full min-h-screen py-16 bg-black bg-opacity-50 ${
+            className={`absolute top-0 z-50 w-full min-h-screen py-16 bg-black bg-opacity-50 animated faster ${
                 hidden ? 'hidden' : ''
-            } ${theme === 'account' ? 'flex flex-row justify-center items-center' : ''}`}
+            } ${animation ? 'fadeIn' : 'fadeOut'} ${
+                theme === 'account' ? 'flex flex-row justify-center items-center' : ''
+            } `}
         >
             {theme !== 'account' && (
                 <div className="relative max-w-6xl px-2 py-12 mx-auto bg-white rounded">
                     <BiX
                         className={`absolute w-8 h-8 cursor-pointer top-2 left-2 ${buttonTheme.get(theme)}`}
-                        onClick={closeEvent}
+                        onClick={modalClose}
                     />
                     {children}
                 </div>
             )}
             {theme === 'account' && (
                 <div className="relative flex flex-col items-center justify-around w-full max-w-lg bg-white rounded h-96 p-14">
-                    <BiX className="absolute w-8 h-8 cursor-pointer top-2 left-2 text-account" onClick={closeEvent} />
+                    <BiX className="absolute w-8 h-8 cursor-pointer top-2 left-2 text-account" onClick={modalClose} />
                     {children}
                 </div>
             )}
