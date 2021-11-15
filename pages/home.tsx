94f129650648775870a3c84f5848d57e12ebf2e8
Nya Candy <dev@candinya.com>
2021-11-15 15:31:09 +0800

    chore: refractor structure

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,17 +1,15 @@
 import type { NextPage } from 'next';
-import React from 'react';
-import AccountItem from '../components/accounts/AccountItem';
-import AssetCard from '../components/assets/AssetCard';
-import FootprintCard from '../components/assets/FootprintCard';
+import React, { useState } from 'react';
 import Button from '../components/buttons/Button';
 import { COLORS } from '../components/buttons/variables';
 import ContentCard from '../components/content/ContentCard';
 import Header from '../components/Header';
 import ImageHolder from '../components/ImageHolder';
-import Profile from '../components/profile/Profile';
 import RecommendSection from '../components/recommends/RecommendSection';
 
 const Home: NextPage = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     let slides = [
         'https://i.imgur.com/GdWEt4z.jpg',
         'https://i.imgur.com/GdWEt4z.jpg',
@@ -43,28 +41,19 @@ const Home: NextPage = () => {
         <>
             <Header>
                 <div className="flex flex-row justify-end w-full gap-x-8">
-                    <Button isOutlined={false} color={COLORS.primary} text={'Create Now'} />
-                    <ImageHolder imageUrl="https://i.imgur.com/GdWEt4z.jpg" isFullRound={true} size={28} />
+                    {isLoggedIn ? (
+                        <>
+                            <Button isOutlined={false} color={COLORS.primary} text={'Create Now'} />
+                            <ImageHolder imageUrl="https://i.imgur.com/GdWEt4z.jpg" isFullRound={true} size={28} />
+                        </>
+                    ) : (
+                        <Button isOutlined={false} color={COLORS.primary} text={'Register Now'} />
+                    )}
                 </div>
             </Header>
             <div className="flex flex-row justify-between max-w-6xl px-2 pt-16 mx-auto gap-x-8">
                 <section className="divide-y-2 w-7/11 divide-solid divide-opacity-5 divide-primary">
-                    <Profile
-                        avatarUrl="https://i.imgur.com/GdWEt4z.jpg"
-                        username="Fendi"
-                        bio={content}
-                        followers={12}
-                        followings={8}
-                        rns="Fendi.rss3.bio"
-                        link="Fendi.github.io"
-                    >
-                        <AccountItem size="sm" chain="BSC" />
-                        <AccountItem size="sm" chain="Ethereum" />
-                        <AccountItem size="sm" chain="Ronin" />
-                        <AccountItem size="sm" chain="Misskey" />
-                        <AccountItem size="sm" chain="Twitter" />
-                    </Profile>
-                    {[...Array(1)].map((_, i) => (
+                    {[...Array(2)].map((_, i) => (
                         <ContentCard
                             key={i}
                             avatarUrl="https://i.imgur.com/GdWEt4z.jpg"
@@ -78,67 +67,6 @@ const Home: NextPage = () => {
                     <div className="w-full py-8 text-sm text-center">That's all :p</div>
                 </section>
                 <section className="flex flex-col gap-4 pb-16 w-4/11">
-                    <div className="grid grid-cols-2 gap-4">
-                        <AssetCard title="NFTs" color="nft" isShowingExpandButton={true} headerButtonMode={'edit'}>
-                            <div className="grid grid-cols-2 gap-3">
-                                {[...Array(4)].map((_, i) => (
-                                    <ImageHolder
-                                        key={i}
-                                        imageUrl={
-                                            'https://rss3.mypinata.cloud/ipfs/QmVFq9qimnudPcs6QkQv8ZVEsvwD3aqETHWtS5yXgdbYY5'
-                                        }
-                                        isFullRound={false}
-                                        size={70}
-                                    />
-                                ))}
-                            </div>
-                        </AssetCard>
-
-                        <AssetCard
-                            title="Donations"
-                            color="donation"
-                            isShowingExpandButton={true}
-                            headerButtonMode={'edit'}
-                        >
-                            <div className="grid grid-cols-2 gap-3">
-                                {[...Array(4)].map((_, i) => (
-                                    <ImageHolder
-                                        key={i}
-                                        imageUrl={
-                                            'https://rss3.mypinata.cloud/ipfs/QmVFq9qimnudPcs6QkQv8ZVEsvwD3aqETHWtS5yXgdbYY5'
-                                        }
-                                        isFullRound={false}
-                                        size={70}
-                                    />
-                                ))}
-                            </div>
-                        </AssetCard>
-                    </div>
-                    <div>
-                        <AssetCard
-                            title="Footprints"
-                            color="footprint"
-                            isShowingExpandButton={true}
-                            headerButtonMode={'edit'}
-                        >
-                            <div className="flex flex-col w-full">
-                                {[...Array(5)].map((_, i) => (
-                                    <FootprintCard
-                                        key={i}
-                                        imageUrl={
-                                            'https://rss3.mypinata.cloud/ipfs/QmVFq9qimnudPcs6QkQv8ZVEsvwD3aqETHWtS5yXgdbYY5'
-                                        }
-                                        startDate={'0x61800f00'}
-                                        endDate={'0x61800f00'}
-                                        city={''}
-                                        country={''}
-                                        username={'RSS3Lover'}
-                                        activity={'Say hi.'}
-                                    />
-                                ))}
-                            </div>
-                        </AssetCard>
-                    </div>
                     <RecommendSection groups={recommendGroups} />
                 </section>
             </div>
