ddf9baa90e20e6a851d811882e0f4d95a4af0e5f
Nya Candy <dev@candinya.com>
2021-11-29 14:54:08 +0800

    chore: optimize rns

--- common/rns.ts
+++ common/rns.ts
@@ -1,159 +1,66 @@
-import { ethers } from 'ethers';
-import { utils } from 'ethers/lib';
 import axios from 'axios';
 
 import config from './config';
 
-type CNAME = 'resolver' | 'token';
-
-let idNo = -1;
-
-async function callRNSContract<T>(
-    cname: CNAME,
-    providerType: 'web3' | 'infura',
-    method: string,
-    ...args: any
-): Promise<T> {
-    let provider: ethers.providers.Web3Provider | ethers.providers.InfuraProvider;
-    let signer; // TODO
-    if (providerType === 'web3') {
-        provider = new ethers.providers.Web3Provider((window as any).ethereum);
-        signer = provider.getSigner();
-    } else {
-        if (idNo === -1) {
-            const poolSize = config.infuraId.length;
-            const idStart = Math.floor(Math.random() * poolSize);
-            for (let i = 0; i < poolSize; i++) {
-                if (await checkInfuraID(config.infuraId[(idStart + i) % poolSize])) {
-                    idNo = (idStart + i) % poolSize;
-                    break;
-                }
-            }
-        }
-
-        provider = new ethers.providers.InfuraProvider(config.rns.test ? 'ropsten' : 'homestead', {
-            projectId: config.infuraId[idNo],
-        });
-    }
-
-    const contract = await new ethers.Contract(
-        getRNSContract(cname),
-        config.rns.contract[cname],
-        signer ? signer : provider,
-    );
-    return contract[method](...args);
-}
-
-async function checkInfuraID(id: string) {
-    try {
-        const res = await axios.post(`https://mainnet.infura.io/v3/${id}`, {
-            jsonrpc: '2.0',
-            id: 1,
-            method: 'eth_accounts',
-            params: [],
-        });
-        if (res.data) {
-            return true;
-        }
-    } catch (e) {
-        console.log(e);
-    }
-    return false;
-}
-
-function getRNSContract(cname: CNAME) {
-    if (config.rns.test) {
-        return config.rns.contractNetworks.ropsten[cname];
-    } else {
-        return config.rns.contractNetworks.mainnet[cname];
-    }
-}
-
-// sha3HexAddress https://eips.ethereum.org/EIPS/eip-181
-function sha3HexAddress(addr: string) {
-    addr = '00' + addr.slice(2);
-    const lookup = '3031323334353637383961626364656600000000000000000000000000000000';
-    let res = '';
-    for (let i = 40; i > 0; i--) {
-        const bit = Number('0x' + addr.slice(i, i + 2)) & 0xf;
-        res = lookup.slice(2 * bit, 2 * bit + 2) + res;
-    }
-    return utils.keccak256('0x' + res);
-}
-
 interface RSS3Domains {
+    address: string;
     rnsName: string;
     ensName: string;
 }
 
-const addrCache: {
-    [key: string]: RSS3Domains;
-} = {};
+const cache: RSS3Domains[] = [];
 
-const nameCache: {
-    [key: string]: string;
-} = {};
+const searchInCache = (aon: string, type: 'address' | 'name') => {
+    let index = -1;
+    if (type === 'address') {
+        index = cache.findIndex((record) => record.address === aon);
+    } else {
+        if (aon.endsWith('.eth')) {
+            index = cache.findIndex((record) => record.ensName === aon);
+        } else {
+            index = cache.findIndex((record) => record.rnsName === aon + config.rns.suffix);
+        }
+    }
+    if (index !== -1) {
+        return cache[index];
+    } else {
+        return null;
+    }
+};
 
 export default {
-    // We have checked network and account in verifyRNS method in RNS.vue, so we don't need to check it here.
-    async register(name: string) {
-        return callRNSContract<ethers.providers.TransactionResponse>('token', 'web3', 'register', name);
-    },
     async addr2Name(addr: string, isPureRNS: boolean = false) {
-        if (addrCache[addr]) {
-            return addrCache[addr].rnsName || addrCache[addr].ensName || '';
+        const cachedValue = searchInCache(addr, 'address');
+        let domainInfo: RSS3Domains | null = null;
+        if (cachedValue) {
+            domainInfo = cachedValue;
         } else {
-            try {
-                const domainInfo = <RSS3Domains>(await axios.get(`https://rss3.domains/address/${addr}`)).data;
-                domainInfo.rnsName = domainInfo.rnsName.replace(config.rns.suffix, '');
-                if (domainInfo) {
-                    addrCache[addr] = domainInfo;
-                }
-                if (isPureRNS) {
-                    return domainInfo.rnsName || '';
-                } else {
-                    return domainInfo.rnsName || domainInfo.ensName || '';
-                }
-            } catch (e) {
-                const reverseNode = '0x91d1777781884d03a6757a803996e38de2a42967fb37eeaca72729271025a9e2';
-                const addrHex = sha3HexAddress(addr.toLowerCase());
-                const node = utils.keccak256(
-                    utils.defaultAbiCoder.encode(['bytes32', 'bytes32'], [reverseNode, addrHex]),
-                );
-                const name = (await callRNSContract<string>('resolver', 'infura', 'name', node))
-                    .toLowerCase()
-                    .replace(config.rns.suffix, '');
-                if (name) {
-                    addrCache[addr] = {
-                        rnsName: name,
-                        ensName: '',
-                    };
-                }
-                return name;
-            }
+            domainInfo = <RSS3Domains>(
+                await axios.get(`/address/${addr}`, {
+                    baseURL: 'https://rss3.domains',
+                })
+            ).data;
+            cache.push(domainInfo);
+        }
+        if (isPureRNS) {
+            return domainInfo.rnsName.replace(config.rns.suffix, '') || '';
+        } else {
+            return domainInfo.rnsName.replace(config.rns.suffix, '') || domainInfo.ensName || '';
         }
     },
     async name2Addr(name: string) {
-        let addr;
-        try {
-            addr = (await axios.get(`https://rss3.domains/name/${name}`)).data.address;
-            return addr;
-        } catch (e) {
-            name = (name + config.rns.suffix).toLowerCase();
-            if (nameCache[name]) {
-                return nameCache[name];
-            } else {
-                const addr = await callRNSContract<string>('resolver', 'infura', 'addr', utils.namehash(name));
-                if (parseInt(addr) !== 0) {
-                    nameCache[name] = addr;
-                }
-                return addr;
-            }
+        const cachedValue = searchInCache(name, 'name');
+        let domainInfo: RSS3Domains | null = null;
+        if (cachedValue) {
+            domainInfo = cachedValue;
+        } else {
+            domainInfo = <RSS3Domains>(
+                await axios.get(`/name/${name}`, {
+                    baseURL: 'https://rss3.domains',
+                })
+            ).data;
+            cache.push(domainInfo);
         }
-    },
-    async balanceOfPass3(addr: string) {
-        await (window as any).ethereum?.enable();
-        const balance = await callRNSContract<ethers.BigNumber>('token', 'infura', 'balanceOf', addr);
-        return Number(ethers.utils.formatUnits(balance, 18));
+        return domainInfo.address || '';
     },
 };
