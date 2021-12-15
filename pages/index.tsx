6ba2b65fdbfb72148c9587027557d0d3e464f3e6
yzhe819 <68207314+yzhe819@users.noreply.github.com>
2021-12-15 14:56:44 +1300

    chore: page loading logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,25 +52,28 @@ const Home: NextPage = () => {
         if (LoginUser.persona || (await RSS3.reconnect())) {
             setLoggedIn(true);
         }
-        const pageOwner = await RSS3.setPageOwner(LoginUser.address);
 
-        const profile = pageOwner.profile;
-        // console.log(pageOwner.assets);
-        if (profile) {
-            // Profile
-            const { extracted, fieldsMatch } = utils.extractEmbedFields(profile?.bio || '', ['SITE']);
-            setAddress(pageOwner?.address || '');
-            setWebsite(fieldsMatch?.['SITE'] || '');
+        if (isLoggedIn) {
+            const pageOwner = await RSS3.setPageOwner(LoginUser.address);
+
+            const profile = pageOwner.profile;
+            // console.log(pageOwner.assets);
+            if (profile) {
+                // Profile
+                const { extracted, fieldsMatch } = utils.extractEmbedFields(profile?.bio || '', ['SITE']);
+                setAddress(pageOwner?.address || '');
+                setWebsite(fieldsMatch?.['SITE'] || '');
+            }
+
+            setTimeout(async () => {
+                const { listed, haveMore } = await utils.initContent('', true);
+                setContent(listed);
+                setHaveMoreContent(haveMore);
+                setContentLoading(false);
+            }, 0);
+
+            setTimeout(initRecommendationGrops, 0);
         }
-
-        setTimeout(async () => {
-            const { listed, haveMore } = await utils.initContent('', true);
-            setContent(listed);
-            setHaveMoreContent(haveMore);
-            setContentLoading(false);
-        }, 0);
-
-        setTimeout(initRecommendationGrops, 0);
     };
 
     const toUserPage = async (addr: string) => {
@@ -254,7 +257,7 @@ const Home: NextPage = () => {
                                     )}
                                 </section>
                             ) : (
-                                <div className="flex flex-col gap-2 w-full py-32 text-sm text-center">
+                                <div className="flex flex-col w-full gap-2 py-32 text-sm text-center">
                                     <p>{'Oops, nothing found from your followings:P'}</p>
                                     <p>{'Check out some new friends from recommendations for you!'}</p>
                                 </div>
@@ -273,8 +276,8 @@ const Home: NextPage = () => {
                     </section>
                 </div>
             ) : (
-                <div className="flex flex-col justify-start max-w-6xl px-2 pt-80 mx-auto gap-y-8 h-full">
-                    <p className="font-semibold text-4xl">This is a closed beta test for Revery and RSS3 v0.3.1.</p>
+                <div className="flex flex-col justify-start h-full max-w-6xl px-2 mx-auto pt-80 gap-y-8">
+                    <p className="text-4xl font-semibold">This is a closed beta test for Revery and RSS3 v0.3.1.</p>
                     <p className="text-xl">Please noted that your profile and data will be deleted after the test.</p>
                 </div>
             )}
