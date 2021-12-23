1d6d4372c283da690f84dfbddc8d77c1411f3937
Nya Candy <dev@candinya.com>
2021-12-23 12:41:03 +0800

    fix: image headers

--- pages/api/imageproxy.ts
+++ pages/api/imageproxy.ts
@@ -4,5 +4,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const url = decodeURIComponent(String(req.query.url));
     const result = await fetch(url);
     const body = await result.body;
+    res.setHeader('Content-Type', result.headers.get('content-type') || 'application/octet-stream');
     await res.send(body);
 }
