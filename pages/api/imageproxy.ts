afaf58b787ec051b7d4312519d68b1688484a347
Nya Candy <dev@candinya.com>
2021-12-25 14:43:39 +0800

    feat: wrap API with sentry

--- pages/api/imageproxy.ts
+++ pages/api/imageproxy.ts
@@ -1,9 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { withSentry } from '@sentry/nextjs';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
     const url = decodeURIComponent(String(req.query.url));
     const result = await fetch(url);
     const body = await result.body;
     res.setHeader('Content-Type', result.headers.get('content-type') || 'application/octet-stream');
     await res.send(body);
 }
+
+export default withSentry(handler);
